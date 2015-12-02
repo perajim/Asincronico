e03c4b72487debf63be44d126716c3311d1e2a97
perajim <perajim@gmail.com>
2015-12-01 22:47:15 -0600

    Segunda version estable

--- Proyecto.m
+++ Proyecto.m
@@ -5,3 +5,18 @@ for i=1:trama
     s = ['Ingrese el valor de la trama: ' num2str(i) ': '];
     names{i} = input(s);
 end
+
+ concatenacion=' ';
+ rods=' ';
+while(true)
+    n=names{i};
+    lar=length(n);
+    while(lar ~= 0)
+        concatenacion =[num2str(concatenacion), n(lar), num2str(i)];
+        lar=lar-1;
+    end
+    break;
+end
+    rods=[num2str(rods),'|', concatenacion,'|'];
+    
+disp(rods);
